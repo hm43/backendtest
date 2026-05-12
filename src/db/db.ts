7d0bd5989890b821ab355c249741b23d0fcda097
Houda Mouttalib <houda.mouttalib@gmail.com>
2026-05-12 16:05:28 +0100

    db diff from prod and local dev environement

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,5 +1,5 @@
-//import { drizzle } from 'drizzle-orm/postgres-js';
 import { drizzle } from 'drizzle-orm/neon-http';
+import { drizzle as pgDrizzle } from 'drizzle-orm/postgres-js';
 import * as schema from '../entities/schema';
 import postgres from 'postgres';
 import { neon } from '@neondatabase/serverless';
@@ -7,10 +7,14 @@ import { neon } from '@neondatabase/serverless';
 const connectionString = process.env.DATABASE_URL!;
 
 let client: any;
+let db: any
 if (process.env.NODE_ENV === 'production') {
     client = neon(connectionString);
+    db = drizzle(client, { schema, logger: true });
+
 } else {
     client = postgres(connectionString, { max: 1 });
+    db = pgDrizzle(client, { schema, logger: true });
 }
 
-export const db = drizzle(client, { schema, logger: false });
+export { db }
