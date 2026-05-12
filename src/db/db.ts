d3cd4c6883210e282f83514881d71bfb200605c4
Houda Mouttalib <houda.mouttalib@gmail.com>
2026-05-12 15:55:39 +0100

    DATABASE_URL correct

--- src/db/db.ts
+++ src/db/db.ts
@@ -3,17 +3,10 @@ import * as schema from '../entities/schema';
 import postgres from 'postgres';
 import { neon } from '@neondatabase/serverless';
 
-console.log('DATABASE_URL:', !!process.env.DATABASE_URL);
-console.log('DATABASE_URL full:', process.env.DATABASE_URL);
-
 const connectionString = process.env.DATABASE_URL!;
 
 let client: any;
 if (process.env.NODE_ENV === 'production') {
-    if (!connectionString?.startsWith('postgresql')) {
-        console.error('Invalid/missing DATABASE_URL:', !!connectionString);
-        throw new Error('DATABASE_URL invalid or missing');
-    }
     client = neon(connectionString);
 } else {
     client = postgres(connectionString, { max: 1 });
