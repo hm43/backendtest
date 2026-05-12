1ae80f3c541087ea74e414f56fb14fd548bd6f50
Houda Mouttalib <houda.mouttalib@gmail.com>
2026-05-12 15:53:07 +0100

    users from neon dstorage

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -4,9 +4,7 @@ import { getUsers } from '../controllers/users';
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-    //const users = await getUsers();
-    const users = await "hello world";
-    console.log("users: ", users);
+    const users = await getUsers();
     res.json(users);
 });
 
